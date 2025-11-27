f641cd7bef03fbfce07e04c470a43d2c13ea0d44
Aaron <2klaus.4peter@gmail.com>
2025-11-28 00:51:25 +0100

    add comments

--- app/crawl.py
+++ app/crawl.py
@@ -5,6 +5,7 @@ import re
 import requests
 
 def get_main_info(soup: BeautifulSoup):
+	'''find student name, degree & title in defense html header'''
 	header = soup.find('h1')
 	if header is None:
 		raise ValueError('Could not find <h1> in the HTML')
@@ -32,6 +33,7 @@ def get_main_info(soup: BeautifulSoup):
 
 
 def parse_examiners(soup:BeautifulSoup):
+	'''find examiners in table of defense html'''
 	table = soup.find('table', {'summary': 'Verantwortliche Dozenten'})
 	if table is None:
 		raise ValueError('Could not find <table summary="Verantwortliche Dozenten"> in the HTML')
@@ -41,11 +43,12 @@ def parse_examiners(soup:BeautifulSoup):
 
 
 def parse_appointment(soup: BeautifulSoup):
+	'''extract appointment start to end datetime + location from defense html'''
 	data = dict(date='-', start='-', end='-', location='-')
 
 	table = soup.find('table', {'summary': 'Übersicht über alle Veranstaltungstermine'})
 	if table is None:
-		return data 
+		return data
 
 	rows = table.find_all('tr')
 	if len(rows) < 2:
@@ -80,6 +83,7 @@ def parse_appointment(soup: BeautifulSoup):
 
 
 def get_defense_data(soup:BeautifulSoup):
+	'''extract key defense data from html'''
 	data = get_main_info(soup)
 	data.update(parse_appointment(soup))
 	data['examiners'] = parse_examiners(soup)
@@ -87,52 +91,61 @@ def get_defense_data(soup:BeautifulSoup):
 
 
 def get_soup(url, log):
+	'''fetch html or load cached defense html'''
+	# check if this is not an overview
 	id_match = re.search(r'publishid=(\d+)', url)
+
+	# load previously crawled defense html from file
+	# (if its not the overview page)
 	if id_match:
 		id = id_match.group(1)
 		filepath = f'./pages/{id}.html'
 
-	if id_match:
-		id = id_match.group(1)
 		if os.path.isfile(filepath):
 			with open(filepath, 'r', encoding='utf-8') as f:
 				text = f.read()
 			text = re.sub(r'&nbsp;', ' ', text) #idk whatever
 			return BeautifulSoup(text, 'html.parser')
 
-	if id_match:
 		log.info(f'fetching publishid {id}...')
+
+	# fetch webpage
 	response = requests.get(url)
 	response.raise_for_status()
 	text = response.text
 
+	# cache defense html (if not the over view page)
 	if id_match:
 		with open(filepath, 'w', encoding='utf-8') as f:
 			f.write(text)
 	text = re.sub(r'&nbsp;', ' ', text) #could also unicodedata.normalize after bs4 parsing
+
+	# return soup
 	return BeautifulSoup(text, 'html.parser')
 
 
-def get_saved_soup(filename):
-	with open(filename, 'r', encoding='utf-8') as f:
-		text = f.read()
-	text = re.sub(r'&nbsp;', ' ', text) #idk whatever
-	return BeautifulSoup(text, 'html.parser')
+# def get_saved_soup(filename):
+# 	with open(filename, 'r', encoding='utf-8') as f:
+# 		text = f.read()
+# 	text = re.sub(r'&nbsp;', ' ', text) #idk whatever
+# 	return BeautifulSoup(text, 'html.parser')
 
 
 def get_defense_urls(soup:BeautifulSoup):
+	'''retrieve defenses urls from overview page'''
 	table = soup.find('table', {'summary': 'Übersicht über alle Veranstaltungen'})
 	defense_entries = table.select('a.regular', href=True)
 	return [a['href'] for a in defense_entries]
 
 
 def get_all_defenses(overview_soup:BeautifulSoup, log):
+	'''crawl or load cached defenses'''
 	defense_urls = get_defense_urls(overview_soup)
 	defenses = {}
 
 	for url in defense_urls:
 		defense_soup = get_soup(url, log)
-		id = re.search(r'publishid=(\d+)', url).group(1) 
+		id = re.search(r'publishid=(\d+)', url).group(1)
 		try:
 			data = get_defense_data(defense_soup)
 			data['url'] = url
@@ -142,20 +155,21 @@ def get_all_defenses(overview_soup:BeautifulSoup, log):
 	return defenses
 
 
-def load_last_crawl(filepath):
-	if os.path.isfile(filepath):
-		with open(filepath, 'r', encoding='utf-8') as f:
-			return json.load(f)
-	return []
+# def load_last_crawl(filepath):
+# 	if os.path.isfile(filepath):
+# 		with open(filepath, 'r', encoding='utf-8') as f:
+# 			return json.load(f)
+# 	return []
 
 
 def save_crawl(defenses, filepath):
+	'''save defenses data as json'''
 	with open(filepath, 'w', encoding='utf-8') as f:
 		json.dump(defenses, f, indent=4)
 
 
-def get_new_defense_items(old_crawl, new_crawl):
-	return {k: v for k, v in new_crawl.items() if k not in old_crawl}
+# def get_new_defense_items(old_crawl, new_crawl):
+# 	return {k: v for k, v in new_crawl.items() if k not in old_crawl}
 
 
 def test_save(url, filename, prettify=False):
@@ -166,22 +180,27 @@ def test_save(url, filename, prettify=False):
 		soup = BeautifulSoup(html, 'html.parser')
 		html = soup.prettify()
 		filename += '_pretty'
-	
+
 	with open(filename, 'w', encoding='utf-8') as f:
 		f.write(html)
-	
+
 
 def main(log):
+	'''do it all from defense fetch till save'''
 	bison_overview_url = 'https://bison.uni-weimar.de/qisserver/rds?state=wtree&search=1&P.vx=kurz&root120252=44747%7C44160%7C43798%7C44232%7C44238&trex=step'
 	json_filepath = 'defenses.json'
+	# create dir for downloaded defense pages
 	os.makedirs('./pages', exist_ok=True)
 
+	# load defenses overview
 	overview_soup = get_soup(bison_overview_url, log)
+	# crawl new defenses
 	new_crawl = get_all_defenses(overview_soup, log)
 	save_crawl(new_crawl, json_filepath)
 	log.info(f'Posts fetched. Total: {len(new_crawl)}')
 
+
 if __name__ == '__main__':
 	bison_overview_url = 'https://bison.uni-weimar.de/qisserver/rds?state=wtree&search=1&P.vx=kurz&root120252=44747%7C44160%7C43798%7C44232%7C44238&trex=step'
 	# test_save(bison_overview_url, 'overview.html', True)
-	main()
+	# main()
--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from jinja2 import Template
 import json
-from fastapi import FastAPI, Response
-from feedgen.feed import FeedGenerator
 import logging
 import markdown
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
+from feedgen.feed import FeedGenerator
 
 import crawl
 
@@ -18,22 +20,28 @@ posts_cache = []
 template_path = './post_template.md'
 
 def load_posts():
+	'''load the posts of the crawled defenses.json to cache'''
 	global posts_cache
 	with open(template_path, 'r') as f:
 		template = f.read()
 	with open('./defenses.json') as f:
 		defenses = json.load(f)
+
 	asc_defenses = sorted(list(defenses.values()), key=lambda d: datetime.strptime(d['date'], "%d.%m.%Y"), reverse=True)
-	posts_cache = [render_template(template, d) for d in asc_defenses]
+	today = datetime.today()
+	new_defenses = [d for d in asc_defenses if datetime.strptime(d['date'], "%d.%m.%Y") >= today]
+	posts_cache = [render_template(template, d) for d in new_defenses]
 	LOG.info(f'Posts reloaded. Total: {len(posts_cache)}')
 
 
 def fetch_defenses():
-	crawl.main(LOG)
+	'''trigger crawling bison & loading posts'''
+	# crawl.main(LOG)
 	load_posts()
 
 
 def render_template(template_str, defense):
+	'''turn defense data to rss format dict'''
 	template = Template(template_str)
 	rendered_md = template.render(**defense)
 
@@ -49,7 +57,8 @@ def render_template(template_str, defense):
 
 @app.on_event('startup')
 def startup_event():
-	fetch_defenses() 
+	'''crawl once on startup, then cron schedule every 6 hours'''
+	fetch_defenses()
 	scheduler = BackgroundScheduler()
 	scheduler.add_job(
 		fetch_defenses,
@@ -67,12 +76,14 @@ def startup_event():
 
 @app.get('/rss.xml')
 def rss_feed():
+	'''serve cached rss'''
 	fg = FeedGenerator()
 	fg.title('Medieninformatik Verteidigungen')
 	fg.link(href='https://bison.uni-weimar.de/qisserver/rds?state=wtree&search=1&trex=step&root120252=44747%7C44160%7C43798%7C44232%7C44238&P.vx=kurz', rel='alternate')
 	fg.description('Aktuelle Informationen über Bachelor- und Masterverteidigungen im Fachbrereich Informatik')
 
 	for post in posts_cache:
+
 		fe = fg.add_entry()
 		fe.title(post['title'])
 		fe.link(href=post['url'])
@@ -82,11 +93,10 @@ def rss_feed():
 	return Response(content=rss_bytes, media_type='application/rss+xml')
 
 
-from fastapi.responses import HTMLResponse
-
 @app.get("/")
 def preview():
-    html = "<h1>Feed Preview</h1>"
-    for post in posts_cache:
-        html += f"<h2><a href='{post['url']}'>{post['title']}</a></h2>{post['content']}"
-    return HTMLResponse(content=html)
+	'''serve html preview of cached rss feed'''
+	html = "<h1>Feed Preview</h1>"
+	for post in posts_cache:
+		html += f"<h2><a href='{post['url']}'>{post['title']}</a></h2>{post['content']}"
+	return HTMLResponse(content=html)
